952cc34766d957f657a549b125baa8ad8c64759b
nicolasmovil <58243363+nicolasmovil@users.noreply.github.com>
2023-10-23 21:37:32 -0300

    audio iniciando el llamado

--- llamar.py
+++ llamar.py
@@ -4,29 +4,31 @@ import sys
 
 # Ejecutar linphonec
 p = subprocess.Popen(["linphonec", "-C"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+subprocess.run(["aplay", "llamando.wav"])
+
 
 # Esperar 3 segundos
 time.sleep(1)
 
 # Mapeo de extensiones
 extension_map = {
-    "1": "4001",
-    "2": "4002",
-    "3": "4003",
-    "4": "4004",
-    "5": "4005",
-    "6": "4006",
-    "7": "4007",
-    "8": "4008",
-    "9": "4009",
-    "0": "4010",
-    "A": "2100",
-    "B": "2200",
-    "C": "2400",
-    "D": "2400",
-    "X": "2500",
-    "E": "3000"
-}
+    "1": "4001",    #
+    "2": "4002",    #
+    "3": "4003",    #
+    "4": "4004",    # 
+    "5": "4005",    # audio tu llamdado no puede completarse
+    "6": "4006",    # audio tu llamdado no puede completarse
+    "7": "4007",    # audio tu llamdado no puede completarse
+    "8": "4008",    # audio tu llamdado no puede completarse
+    "9": "4009",    # audio tu llamdado no puede completarse
+    "0": "4010",    # no anda el boton
+    "A": "2100",    # audio tu llamdado no puede completarse
+    "B": "2200",    # audio tu llamdado no puede completarse
+    "C": "2400",    # llamada a pc
+    "D": "2400",    # todos 
+    "X": "2500",    # audio tu llamdado no puede completarse
+    "E": "3000"     # audio tu llamdado no puede completarse
+} 
 
 # Leer el atributo con el que se ejecutÃ³ la aplicaciÃ³n
 if len(sys.argv) == 2:
